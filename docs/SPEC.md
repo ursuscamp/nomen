8abbb443e70c776f88325ba9cf13b4eeb48ea355
Ryan Breen <ryan@breen.xyz>
2023-02-26 23:13:36 -0500

    Working on a spec.

---
+++ docs/SPEC.md
@@ -0,0 +1,31 @@
+# GUNS
+
+## Global Uncensorable Name System
+
+### Specification
+
+#### Introduction
+
+The following lay out the specification for GUNS, the Global Uncensorable Name System. It is designed in the hopes that it will provide a global name system without the need for a centralized organization like ICANN with final control authority.
+
+#### Workflow
+
+##### Initiation Transaction
+
+In order to initiate a new namespace, a `NAMESPACE_ID` must be commited to the Bitcoin blockchain in an OP_RETURN output (output value may be `0`). This is called an __initiation transaction__.
+
+The initiation transaction must take this form: `OP_RETURN GUN \x00 \x00 NAMESPACE_ID`.
+
+The null byte `\x00` following `GUN` is a version byte. It's made to indicate which version of the parser to user for the following information. Currently, version `0` is the only version, but potential future protocol revisions should increment by 1.
+
+The next null byte is a __transaction type__. The transaction type indicates indicates which transaction will follow. Transsaction type `0` indicates a namespace intitiation transaction.
+
+The `NAMESPACE_ID` is a 20-byte hash value of the namespace data. It is created by concatenating the following values: 32-byte secp256k1 public key, 32-byte merkle root for namespace data, and the UTF-8 encoded __namespace root__, or what we would typically think of a top-level name. Then the value is run through the HASH160 algorithm like so: `RIPEMD160(SHA256(PUBKEY MERKLE_ROOT NAMESPACE_ROOT))`. The 20-byte value produced by this function is the `NAMESPACE_ID`, which uniquely identifies a root name.
+
+Namespace names's must obey the following limitations:
+
+* Must be 256 bytes or less (restriction may be lifted in later versions).
+* Must be valid UTF-8 strings.
+* Must NOT begin with underscore character `_`. All names beginning with underscore characters are reserved for protocol use.
+
+During indexing, a client must ignore any transactions where any names violate these rules. For the purposes of the protocol, the invalid transaction may as well not exist. In the case of an initiation transaction, for example, if a namespace root is valid, but a child name is not valid, the entire transaction is invalid, and the namespace root is still considered "free" to be claimed by anyone.
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,46 @@
 #![allow(unused)]
 
-use std::str::FromStr;
+use std::{borrow::BorrowMut, str::FromStr};
 
 use bitcoin::{
-    hashes::hex::FromHex, psbt::PartiallySignedTransaction, Address, OutPoint, PackedLockTime,
-    Script, Sequence, Transaction, TxIn, TxOut, Txid, Witness,
+    blockdata::{
+        opcodes::{
+            all::{OP_ENDIF, OP_IF},
+            OP_FALSE,
+        },
+        script::Builder,
+    },
+    hashes::hex::FromHex,
+    psbt::{serialize::Deserialize, PartiallySignedTransaction, Psbt},
+    Address, OutPoint, PackedLockTime, Script, Sequence, Transaction, TxIn, TxOut, Txid, Witness,
 };
-use bitcoincore_rpc::{Auth, Client, RpcApi};
+use bitcoincore_rpc::{Auth, Client, RawTx, RpcApi};
 
 static AUTH: &'static str = include_str!("../.auth");
 
-static TXID: &'static str = "473cbaaa0ac29a07a3ac14a46effc3df9c341083417b78378e0ae58d7daf6ffb";
+static TXID: &'static str = "579807220e5759bb738c067ba279dd886b167f9a505242bf1329f57e677ce605";
 static INPUT_VALUE: u64 = (50 * 100_000_000);
 static OUTPUT_VALUE: u64 = INPUT_VALUE - 1000;
 static VOUT: u32 = 0;
-static ADDR: &'static str = "bcrt1qjydp0w246juddlpqsdzn7st43uhtg69ue5clv3";
+static ADDR: &'static str = "bcrt1qqssfvug98rf6f668a2x6rr47dxdhymrrpt4z07";
 
 fn main() {
+    // let script = Builder::new()
+    //     .push_opcode(OP_FALSE)
+    //     .push_opcode(OP_IF)
+    //     .push_slice("hello world".as_bytes())
+    //     .push_opcode(OP_ENDIF)
+    //     .into_script();
+
+    let script = Script::new_op_return("hello_world".as_bytes());
+
     let outpoint = OutPoint::new(Txid::from_hex(TXID).unwrap(), VOUT);
 
     let txin = TxIn {
         previous_output: outpoint,
         script_sig: Script::new(),
         sequence: Sequence::MAX,
-        witness: Witness::default(),
+        witness: Witness::new(),
     };
 
     let txout = TxOut {
@@ -31,15 +48,20 @@ fn main() {
         script_pubkey: Address::from_str(ADDR).unwrap().script_pubkey(),
     };
 
+    let op_return = TxOut {
+        value: 0,
+        script_pubkey: script,
+    };
+
     let tx = Transaction {
         version: 2,
         lock_time: PackedLockTime::ZERO,
         input: vec![txin],
-        output: vec![txout],
+        output: vec![txout, op_return],
     };
 
-    let psbt = PartiallySignedTransaction::from_unsigned_tx(tx).unwrap();
-    println!("Psbt: {psbt}");
+    let txh = tx.raw_hex();
+    println!("Transaction: {txh}");
 }
 
 fn auth() -> (&'static str, Auth) {
