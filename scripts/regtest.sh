8819fbab994255d7ef447efddfb5499237f96c08
Ryan Breen <ryan@breen.xyz>
2023-03-11 06:49:08 -0500

    Updating scripts to be more helpful.

--- scripts/regtest.sh
+++ scripts/regtest.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+CREDS="-regtest -rpcuser=regtest -rpcpassword=regtest"
+
+while getopts r flag
+do
+    case "${flag}" in
+        r) rm -rf .bitcoin;;
+    esac
+done
 
 mkdir -p .bitcoin
-bitcoind -regtest -datadir=.bitcoin -rpcuser=regtest -rpcpassword=regtest -txindex
+bitcoind -daemon -datadir=.bitcoin -regtest $CREDS -txindex
+sleep 1.5
+bitcoin-cli $CREDS createwallet regtest
+ADDRESS=$(bitcoin-cli $CREDS -rpcwallet=regtest getnewaddress)
+echo "$ADDRESS"
+bitcoin-cli $CREDS -rpcwallet=regtest generatetoaddress 101 $ADDRESS
+tail -f .bitcoin/regtest/debug.log
