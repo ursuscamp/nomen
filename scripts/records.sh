1952c8572c7c4f2ef277c4896613df4c0008a051
Ryan Breen <ryan@breen.xyz>
2023-05-20 22:24:11 -0400

    Remove scripts from repo.

--- scripts/records.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-CREDS="-regtest -rpcuser=regtest -rpcpassword=regtest"
-CMD="bitcoin-cli $CREDS -rpcwallet=regtest"
-
-PRIVKEY="f5daf17ccf02488bc0ab506fc550016963af3030d4c5d2b7b3e3c232f3c0d7ca"
-PUBKEY="d57b873363d2233d3cd54453416deff9546df50d963bb1208da37f10a4c23d6f"
-
-RUST_LOG=off cargo run -- name record --privkey $PRIVKEY smith IP4=127.0.0.1 NPUB=npub1234
--- scripts/records_transfer.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-CREDS="-regtest -rpcuser=regtest -rpcpassword=regtest"
-CMD="bitcoin-cli $CREDS -rpcwallet=regtest"
-
-PRIVKEY="23dfe0450af72a460acb5322372b43265885facca7b2539bb8c568c432068820"
-PUBKEY="cb5dd62f5018ddd6dc9c49b492b20c78dc3c84fc7f237b101334c5aed2bb6247"
-
-RUST_LOG=off cargo run -- name record --privkey $PRIVKEY smith IP4=transfer NPUB=transfer
